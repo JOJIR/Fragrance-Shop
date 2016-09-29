cba95663b94c6bd0941f6945c012bdeae9f87857
JOJIR <jojiachu5@gmail.com>
2016-09-29 23:49:57 +0530

    is admin changes to role,grid controller is added
    is admin changes to role,grid controller is added

--- ShoppingCart/src/main/java/com/powerperfume/controller/Logincontroller.java
+++ ShoppingCart/src/main/java/com/powerperfume/controller/Logincontroller.java
@@ -11,44 +11,44 @@ import org.springframework.web.servlet.ModelAndView;
 import com.powerperfume.dao.UserDAO;
 import com.powerperfume.model.User;
 
+
 @Controller
-public class Logincontroller {
+public class LoginController{
+
 	@Autowired
 	UserDAO userdao;
 	
 	@RequestMapping("/Login")
-	public ModelAndView login(@ModelAttribute("user")User user)
+	public ModelAndView login(@ModelAttribute("user") User user)
 	{
 		return new ModelAndView("Login");
 	}
 	
-	@RequestMapping(value="/LoginAttempt",method=RequestMethod.POST)
-	public ModelAndView loginAttempt(@ModelAttribute("user")User user,ModelMap model)
+	@RequestMapping(value="/LoginAttempt", method = RequestMethod.POST)
+	public ModelAndView loginAttempt(@ModelAttribute("user") User user, ModelMap model)
 	{
 		ModelAndView modelview = null;
 		
-		user.setAdmin(false);
-		if(userdao.isValidUser(user))
+		user = userdao.getValidUser(user);
+		if(user != null)
 		{
-			modelview = new ModelAndView("UserHome");
-					modelview.addObject("email",user.getEmail());
-					
-					return modelview;
+			if(user.getRole().equals("ROLE_USER"))
+			{
+				modelview = new ModelAndView("UserHome");
+				modelview.addObject("email", user.getEmail());
+			}
+			else if(user.getRole().equals("ROLE_ADMIN"))
+			{
+				modelview = new ModelAndView("AdminHome");
+			}
 		}
-		
-		user.setAdmin(true);
-		if(userdao.isValidUser(user))
-		{
-			modelview = new ModelAndView("AdminHome");
-					
-		}
-		
 		else
 		{
 			modelview = new ModelAndView("InvalidCredentials");
 		}
+
+
 		return modelview;
 	}
-	
 }
 
